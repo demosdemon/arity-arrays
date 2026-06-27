0a89aed471911b969085d3164c004b822c623111
Brandon LeBlanc <brandon@leblanc.codes>
2026-06-26 17:21:45 -0700

    chore: stub the designed crates

--- crates/arity-arrays/src/lib.rs
+++ crates/arity-arrays/src/lib.rs
@@ -1,3 +1,9 @@
-pub mod bitmap;
-pub mod dense;
-pub mod sparse;
+#![no_std]
+
+extern crate alloc;
+
+pub mod fixed;
+pub mod packed;
+
+pub use arity_bitmap as bitmap;
+pub use arity_index as index;
